c9b8a6a39474b39be4490699d1bdb6c94d4abe07
Michael Wheeler <wheeler.m@husky.neu.edu>
2020-03-12 15:58:08 -0400

    Follow to next spider class

--- config.py
+++ config.py
@@ -5,3 +5,5 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).parent
 DATA_DIR = BASE_DIR / "data"
+
+WRITE_TEMP_PROGRESS = True
--- preprocessing/genius/genius/spiders/artists.py
+++ preprocessing/genius/genius/spiders/artists.py
@@ -2,7 +2,7 @@ from csv import writer
 import scrapy
 from string import ascii_lowercase
 
-from config import DATA_DIR
+from config import DATA_DIR, WRITE_TEMP_PROGRESS
 
 
 class ArtistsDirectorySpider(scrapy.Spider):
@@ -22,18 +22,31 @@ class ArtistsDirectorySpider(scrapy.Spider):
         artist_names = artists.xpath("./text()").getall()
         artist_links = artists.xpath("./@href").getall()
         artist_info = zip(artist_names, artist_links)
-        with open(DATA_DIR / f'artists_all_{letter}.csv', "w") as f:
-            csv_writer = writer(f)
-            for artist_tuple in artist_info:
-                csv_writer.writerow(artist_tuple)
-        # TODO -- follow the artists links via ArtistPageSpider
+        
+        # Save progress to CSV file
+        if WRITE_TEMP_PROGRESS:
+            with open(DATA_DIR / f'artists_all_{letter}.csv', "w") as f:
+                csv_writer = writer(f)
+                for artist_tuple in artist_info:
+                    csv_writer.writerow(artist_tuple)
+        
+        for artist_name, artist_link in artist_info:
+            yield scrapy.Request(url=artist_link, callback=ArtistPageSpider.parse,
+                                 cb_kwargs={"artist_name": artist_name})
 
 
 class AritstPageSpider(scrapy.Spider):
     name = "artist"
     
-    def parse(self, response):
-        # TODO -- get meta content="/artists/***" with name="newrelic-resource-path
+    def parse(self, response, artist_name=None):
+        # Pull artist ID from meta tag in artist page
+        artist_id = response.xpath('//head/meta[@name="newrelic-resource-path"]/@content')
+        artist_id = artist_id.lstrip("/artists/")
+        
+        # Save progress to text file
+        if WRITE_TEMP_PROGRESS:
+            with open(DATA_DIR / f'id_{artist_name}.data',  "w") as f:
+                f.write(f'{artist_name}, {artist_id}')
+        
         # TODO -- hit https://genius.com/api/artists/**/songs?page=1, then paginate
         # TODO -- follow those links via a SongLyric Scraper
-        pass
